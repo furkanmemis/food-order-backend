386bc066969fd3f553580b03e81f41dd686dafd4
furkanmemis <fmemis0160@gmail.com>
2025-02-22 15:06:38 +0300

    Added cors policy

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const connectDB = require('./config/dbConnection')
 const { initAdmin } = require('./initilization/adminInitilization'); // Doğru yol
 const authenticationRoute = require('./routes/authentication');
 const userRoute = require('./routes/user');
+const cors = require('cors');
 
 connectDB();
 initAdmin();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use('/auth',authenticationRoute);
 app.use('/user',userRoute);
